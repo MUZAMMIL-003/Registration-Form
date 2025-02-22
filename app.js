e0e415a442c347b6ec6690b6cce983a8bf95e4f1
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-02-22 23:57:16 +0500

    .

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ import {
 } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js'
 
 import {
-    collection,
-    getDocs,
-    query,
-     where,
-} from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js'
-import {
     auth, db,
 } from "./firebaseConfig.js";
 
@@ -22,6 +16,7 @@ if (!User) {
 console.log(User);
 
 let userName = localStorage.getItem("userName");
+console.log(userName)
 
 ////checking user state
 onAuthStateChanged(auth, (user) => {
@@ -49,33 +44,7 @@ onAuthStateChanged(auth, (user) => {
 
 
 
-/// getting All posts from firebase
-const querySnapshot = await getDocs(collection(db, "Posts"));
-querySnapshot.forEach((doc) => {
 
-    let user = doc.data().Userid;
-    // console.log("user-Id =>", user)
-    let posts = doc.data().post;
-    // console.log("user-text =>", posts)
-    let email = doc.data().email;
-    let username = doc.data().userName /// yaha mujhe userName get krna he front me show karwaney k liyee...
-    console.log("user-Name =>", username)
-
-    
-    let rowDiv = document.querySelector(".row");
-    let tempDiv = `
-    <div class="col  ">
-    <div class="card p-3 bg-info bg-opacity-10 border border-info border rounded border-light h-100" h-100">
-    <div class="card-body  ">
-    <h5 class="card-title  text-light">${posts}</h5>
-    <p class="card-text text-light">Written By => ${ username}</p>
-    </div>
-    </div>
-    </div>
-    `
-
-    rowDiv.innerHTML += tempDiv;
-});
 
 
 
@@ -85,7 +54,7 @@ let signOutBttn = () => {
     signOut(auth).then(() => {
         console.log("log out successfully");
         localStorage.removeItem("user");
-        window.location.assign("./simple sign up login/registration page/register.html");
+        window.location.assign("./simple sign up login/login.html");
     }).catch((error) => {
         console.log(error.message);
     });
